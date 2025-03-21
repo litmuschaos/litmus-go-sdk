b22da1022c938e654a7b99a081943222aa302aa1
Sky Singh <akashsingh2210670@gmail.com>
2025-03-21 16:47:00 +0530

    Removed unused functions from `common.go`
    Signed-off-by: Sky Singh <akashsingh2210670@gmail.com>

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -16,107 +16,15 @@ limitations under the License.
 package utils
 
 import (
-	"bufio"
 	"bytes"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"io"
-	"math/big"
 	"net/http"
-	"os"
-	"regexp"
-	"strings"
 
 	"github.com/litmuschaos/litmus-go-sdk/pkg/logger"
 	"github.com/litmuschaos/litmus-go-sdk/pkg/types"
-	"gopkg.in/yaml.v2"
 )
-
-func Scanner() string {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		return scanner.Text()
-	}
-	if err := scanner.Err(); err != nil {
-		logger.Errorf("reading standard input: %v", err)
-	}
-	return ""
-}
-
-func PrintError(err error) {
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-}
-
-func PrintInJsonFormat(inf interface{}) {
-	var out bytes.Buffer
-	byt, err := json.Marshal(inf)
-	PrintError(err)
-
-	err = json.Indent(&out, byt, "", "  ")
-	PrintError(err)
-
-	logger.Info(out.String())
-}
-
-func PrintInYamlFormat(inf interface{}) {
-	byt, err := yaml.Marshal(inf)
-	PrintError(err)
-
-	logger.Info(string(byt))
-}
-
-func GenerateRandomString(n int) (string, error) {
-	if n <= 0 {
-		return "", fmt.Errorf("length should not be negative")
-	}
-	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
-	ret := make([]byte, n)
-	for i := 0; i < n; i++ {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
-		if err != nil {
-			return "", err
-		}
-		ret[i] = letters[num.Int64()]
-	}
-
-	return string(ret), nil
-}
-
-func CheckKeyValueFormat(str string) bool {
-	selectors := strings.Split(str, ",")
-
-	for _, el := range selectors {
-		kv := strings.Split(el, "=")
-		if len(kv) != 2 {
-			logger.Error("nodeselector is not correct. Correct format: \"key1=value2,key2=value2\"")
-			return false
-		}
-
-		if strings.Contains(kv[0], "\"") || strings.Contains(kv[1], "\"") {
-			logger.Error("nodeselector contains escape character(s). Correct format: \"key1=value2,key2=value2\"")
-			return false
-		}
-	}
-	return true
-}
-
-func GenerateNameID(in string) string {
-	// Replace spaces and special characters with underscore
-	reg := regexp.MustCompile(`[^a-zA-Z0-9]+`)
-	replaced := reg.ReplaceAllString(in, "_")
-
-	// Remove hyphens
-	noHyphens := strings.ReplaceAll(replaced, "-", "")
-
-	// Convert everything to lowercase
-	nameID := strings.ToLower(noHyphens)
-
-	return nameID
-}
 
 // SendHTTPRequest is a utility function to send HTTP requests and handle common response patterns
 func SendHTTPRequest(endpoint, token string, payload []byte, method string) ([]byte, error) {
